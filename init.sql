5fc9697027ad4a448e9f034e61d06c471687d347
minseok <minseokgyang@gmail.com>
2025-10-15 21:37:01 +0900

    commit1

--- init.sql
+++ init.sql
@@ -12,3 +12,13 @@ CREATE TABLE IF NOT EXISTS schedules (
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 INSERT INTO `schedule_db`.`schedules` (`title`, `description`, `start_datetime`, `end_datetime`, `is_completed`) VALUES ("풋살", "정기 풋살 모임", '20251015080000', '20251015090000', '1');
+INSERT INTO `schedule_db`.`schedules` (`title`, `description`, `start_datetime`, `end_datetime`, `is_completed`) VALUES("영화보기","체인소맨",20251002200000,20251003010000);
+INSERT INTO `schedule_db`.`schedules` (`title`, `description`, `start_datetime`, `end_datetime`, `is_completed`) VALUES("영화보기","귀멸의칼날",20251010100000,20251010130000);
+INSERT INTO `schedule_db`.`schedules` (`title`, `description`, `start_datetime`, `end_datetime`, `is_completed`) VALUES("게임하기","종합게임",20251003000000,20251011050000);
+INSERT INTO `schedule_db`.`schedules` (`title`, `description`, `start_datetime`, `end_datetime`, `is_completed`) VALUES("공부하기","flask",20251011060000,20251011061000);
+INSERT INTO `schedule_db`.`schedules` (`title`, `description`, `start_datetime`, `end_datetime`, `is_completed`) VALUES("공부하기","파이썬",20251011061000,20251011062000);
+INSERT INTO `schedule_db`.`schedules` (`title`, `description`, `start_datetime`, `end_datetime`, `is_completed`) VALUES("공부하기","django",20251011062000,20251011063000);
+INSERT INTO `schedule_db`.`schedules` (`title`, `start_datetime`, `end_datetime`, `is_completed`) VALUES("낮잠자기",20251011062000,20251011132000);
+INSERT INTO `schedule_db`.`schedules` (`title`, `start_datetime`, `end_datetime`, `is_completed`) VALUES("운동하기",20251011133000,20251011192000);
+INSERT INTO `schedule_db`.`schedules` (`title`, `start_datetime`, `end_datetime`, `is_completed`) VALUES("낮잠자기",20251011192000,20251011212000);
+INSERT INTO `schedule_db`.`schedules` (`title`, `description`, `start_datetime`, `end_datetime`, `is_completed`) VALUES("일본어공부하기","N3",20251011212000,20251011212000);
--- schedule_manager.py
+++ schedule_manager.py
@@ -12,15 +12,62 @@ def get_db_connection() -> pymysql.Connection:
 
 def add_schedule(cursor: Cursor):
     # Todo: 여기에 일정 추가 코드를 작성합니다.
+    # 일정 정보를 입력받는다.
+    title = input("제목: ")
+    desc = input("설명 : ")
+    start_datetime = input("시작시간(yyyymmddhhmmss)")
+    end_datetime = input("종료시간(yyyymmddhhmmss)")
+
+    #Insert 쿼리 작성
+    sql = """
+        INSERT INTO schedules(title, description, start_datetime, end_datetime)
+        VALUES(%s,%s,%s,%s)
+    """
+
+    #INSERT 쿼리 실행
+    cursor.execute(sql, (title, desc, start_datetime, end_datetime))
+    print("일정이 추가됨.")
     raise NotImplementedError("함수 미구현")
 
 def get_schedules(cursor: Cursor):
     # Todo: 여기에 일정 정보를 가져오는 코드를 작성합니다.
+    #select 쿼리를 작성한다.
+    sql = "SELECT id, title, description, start_datetime, is_completed FROM schedules"
+
+    #select쿼리에서 실행된 튜플을 가져오기.
+    cursor.execute(sql)
+    schedules = cursor.fetchall()
+    if not schedules:
+        print("등록된 일정이 없습니다.")
+        return
+
+    #가져온 튜플을 출력한다.
+    for schedule in schedules:
+        id ,title, description, start_datetime, is_completed = schedule
+        status = "완료" if is_completed else "미완료"
+
+        print(f"[{id}] {title}")
+        print(f"    상태:{status}")
+        print(f"    상태:{start_datetime}")
+        if description:
+            print(f"    설명: {description}")
+        print()
+
     raise NotImplementedError("함수 미구현")
 
 def complete_schedule(cursor: Cursor):
     # Todo: 여기에 일정을 완료처리하는 코드를 작성합니다.
-    raise NotImplementedError("함수 미구현")
+
+        #완료 시킬 일정의  id값을 입력한다.
+        id = input("id값을 입력하시오.")
+
+        #UPdate쿼리를 작성한다.
+        sql = "UPDATE schedules SET is_completed = true where id = %s"
+
+        #UPdate쿼리를 실행한다.
+        cursor.execute(sql, id)
+        print("일정이 완료됨.")
+        raise NotImplementedError("함수 미구현")
 
 # 메뉴 나타내기
 def show_menu() -> str:
